331205b29ff46a1c9e1d5e574787f7c368555dd2
Josue Neis <jneis@github.com>
2016-02-04 10:41:34 -0200

    testing controller & scope

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,19 @@
 var app = angular.module('utaApp', []);
 
 app.controller('ctrl', function($rootScope, $scope) {
-    $scope.title = 'Udemy\'s Unit Testing AngularJS'
+    $scope.title = 'Udemy\'s Unit Testing AngularJS';
+
+    $scope.destinations = [];
+
+    $scope.destination = {
+        city: undefined,
+        country: undefined
+    };
+
+    $scope.add = function() {
+        $scope.destinations.push({
+            city: $scope.destination.city,
+            country: $scope.destination.country
+        });
+    };
 });
--- app/js/test/unit/appUnitSpec.js
+++ app/js/test/unit/appUnitSpec.js
@@ -2,32 +2,48 @@
 //    (suite name, suite function)
 describe('app-test-suite', function() {
 
+    // angular-mocks' 'module' includes target module into test
+    beforeEach(module('utaApp'));
+
     // testing controller
     describe('controller-test', function() {
+        var scope, ctrl;
+
+        beforeEach(
+            // angular-mocks' 'inject' injects components to test
+            //    in this case, injected component is a controller
+            inject(function($controller, $rootScope) {
+                scope = $rootScope.$new();
+                // (controller name, scope link)
+                ctrl = $controller('ctrl', {$scope: scope});
+            })
+        );
 
         //  jasmine's 'it' defines a single unit test
         //    (test name, test function)
         it('should initialize the title', function() {
-
-            // angular-mocks' 'module' includes target module into test
-            module('utaApp');
-
-            // initializes the scope
-            var scope = {};
-            var ctrl;
-
-            // angular-mocks' 'inject' injects components to test
-            //    in this case, injected component is a controller
-            inject(function($controller) {
-                // (controller name, scope link)
-                ctrl = $controller('ctrl', {$scope: scope});
-            });
 
             // jasmine's 'toBeDefined' matcher
             expect(scope.title).toBeDefined();
 
             // jasmine's 'toBe' matcher
             expect(scope.title).toBe('Udemy\'s Unit Testing AngularJS');
+        });
+
+        it('should add destination to list', function() {
+            expect(scope.destinations).toBeDefined();
+            expect(scope.destinations.length).toBe(0);
+
+            scope.destination = {
+                city: 'London',
+                country: 'UK'
+            };
+
+            scope.add();
+
+            expect(scope.destinations.length).toBe(1);
+            expect(scope.destinations[0].city).toBe('London');
+            expect(scope.destinations[0].country).toBe('UK');
         });
 
     });
